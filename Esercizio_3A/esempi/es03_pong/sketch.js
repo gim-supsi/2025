702e0b17b3a21173189e55358a21c01df0bfe93f
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-05-06 16:35:50 +0200

    Update sketch.js

--- Esercizio_3A/esempi/es03_pong/sketch.js
+++ Esercizio_3A/esempi/es03_pong/sketch.js
@@ -8,10 +8,10 @@ function setup() {
 	createCanvas(500, 400)
 
 	posX = 200
-	velX = 7
+	velX = 3
 
 	posY = 100
-	velY = 4
+	velY = 2
 
 	background(255,0,0)
 }
@@ -41,8 +41,12 @@ function draw() {
 	//background(190)
 
 	noStroke()
-	fill(frameCount % 2 * 255)
-	let d = map(sin(frameCount * 0.1), -1, 1, 10, 200)
+	let d = map(sin(frameCount * 0.11), -1, 1, 20, 100)
+	let r = map(sin(frameCount * 0.13), -1, 1, 0, 255)
+	let g = map(sin(frameCount * 0.14), -1, 1, 0, 255)
+	let b = map(sin(frameCount * 0.16), -1, 1, 0, 255)
+	// fill(frameCount % 2 * 255)
+	fill(r, g, b)
 	ellipse(posX, posY, d)
 
 
